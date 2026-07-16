c53125cb28d1e6d46f2d0abfee768a12f8e4a563
MD. FAYSAL KHAN <22-49312-3@student.aiub.edu>
2026-07-16 13:08:50 +0600

    Feat: implemented nativewind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
+import "../../global.css"
+export default function RootLayout() {
+  return <Stack>
+    <Stack.Screen name="index" options={{ title: "Home" }}/>
+    <Stack.Screen name="about" options={{ title: "About" }} />
+  </Stack> 
+  
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
 }
