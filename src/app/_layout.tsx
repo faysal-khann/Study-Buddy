8557b037b868de03ec9ece215fdb622749b5575b
MD. FAYSAL KHAN <22-49312-3@student.aiub.edu>
2026-07-18 20:48:25 +0600

    Stream Chat setup & expo api route
    Refereance:
    Stream: https://getstream.io/chat/sdk/react-native/tutorial/expo/
    API Routes:https://docs.expo.dev/router/web/api-routes/

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,12 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import * as Sentry from "@sentry/react-native";
 import { Stack } from "expo-router";
-import "../../global.css"
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import * as Sentry from '@sentry/react-native';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "../../global.css";
 
 Sentry.init({
-  dsn: 'https://bb86ece0e9923d4b8361647551453c1e@o4511740524429312.ingest.us.sentry.io/4511751633502208',
+  dsn: "https://bb86ece0e9923d4b8361647551453c1e@o4511740524429312.ingest.us.sentry.io/4511751633502208",
 
   // Adds more context data to events (IP address, cookies, user, etc.)
   // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
@@ -17,23 +18,23 @@ Sentry.init({
   // Configure Session Replay
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: __DEV__,
 });
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}
-     publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
-    >
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="(auth)" />
-
-      </Stack>
+    <ClerkProvider tokenCache={tokenCache}>
+      <GestureHandlerRootView  className="flex-1">
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      </GestureHandlerRootView>
     </ClerkProvider>
-  )
-  
-
+  );
 }
