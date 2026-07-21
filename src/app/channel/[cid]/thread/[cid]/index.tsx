1d30f8f54a6abb9fa341986539791c016142495e
MD. FAYSAL KHAN <22-49312-3@student.aiub.edu>
2026-07-21 10:50:36 +0600

    simple fix

--- src/app/channel/[cid]/thread/[cid]/index.tsx
+++ src/app/channel/[cid]/thread/[cid]/index.tsx
@@ -1,11 +1,46 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import { useAppContext } from '@/contexts/AppProvider';
+import { useHeaderHeight } from '@react-navigation/elements';
+import { FullScreenLoader } from '@/components/FullScreenLoader';
+import { Channel, MessageInput, MessageList, Thread, useChatContext } from "stream-chat-expo";
+import {SafeAreaView} from 'react-native-safe-area-context';
+import { EmptyState } from '@/components/EmptyState';
 const ThreadScreen = () => {
+
+  const { channel, thread, setThread } = useAppContext();
+  const headerHeight = useHeaderHeight();
+
+  if (channel === null) {
+    return <FullScreenLoader message="Channel not found" />;
+  }
+
+
+
   return (
-    <View>
-      <Text>ThreadScreen</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-surface'>
+       <Channel
+              channel={channel}
+              keyboardVerticalOffset={headerHeight}
+              thread={thread}
+              threadList
+              EmptyStateIndicator={() => (
+                <EmptyState
+                  icon="chatbubbles-outline"
+                  title="No messages yet"
+                  subtitle="Start the conversation by sending a message."
+                />
+              )}
+            >
+
+              <View className="flex-1 justify-start">
+                <Thread 
+                  onThreadDismount={() => setThread(null)}
+                />
+              </View>
+
+            </Channel>
+    </SafeAreaView>
   )
 }
 
